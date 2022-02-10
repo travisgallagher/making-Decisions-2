d9022a031b0d9f58570e28d892552a9ae00a502b
travisgallagher <travisgallagher52@gmail.com>
2022-02-10 15:55:41 -0700

    Questions 1-11 complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -152,11 +152,15 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+for(let i = letters.length - 1; i >= 0; i--) {
+  console.log(letters[i]);
+};
 
 ////////// Advanced Problems //////////
 
 ////////// PROBLEM 10 //////////
-// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
+// Switch statements can be excellent alternatives to if blocks. 
+// Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
 
 // Do not edit the code below.
 let letterGrade = 'B'
@@ -174,11 +178,31 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
+switch (letterGrade) {
+  case `A`: console.log('The student is doing excellently.')
+  break; 
+  case `B`: console.log('The student is doing well.')
+  break; 
+  case `C`: console.log('The student is doing alright.')
+  break; 
+  case `D`: console.log('The student is not doing very well.')
+  break; 
+  case `F`: console.log('The student is failing.')
+  break; 
+  default: console.log('Not an eligible grade.')
+}; 
+
+
+
+
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, 
+  if the number your for loop is currently on is divisible by 3, console.log 'Dev'.
+   If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+   If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
 
   Your output should look like:
   1
@@ -201,3 +225,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('Devmountain');
+  } else if (i % 5 === 0) {
+    console.log('mountain');
+  } else if (i % 3 === 0) {
+    console.log('Dev');
+  } else {
+    console.log(i);
+  };
+};
